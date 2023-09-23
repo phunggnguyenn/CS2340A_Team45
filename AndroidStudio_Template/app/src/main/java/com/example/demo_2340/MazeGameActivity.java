e234e5cc9617a59b8032c75779decda7335b9639
Maria Jothish <mariaphilipj@gmail.com>
2023-09-23 15:32:14 -0400

    Display and update difficulty in MazeGameActivity

--- AndroidStudio_Template/app/src/main/java/com/example/demo_2340/MazeGameActivity.java
+++ AndroidStudio_Template/app/src/main/java/com/example/demo_2340/MazeGameActivity.java
@@ -1,12 +1,26 @@
 package com.example.demo_2340;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 //launchMazeGameActivity() method in GameActivity.java will launch the maze background
 public class MazeGameActivity extends AppCompatActivity {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze); //will use the activity_maze xml background
+        textView = (TextView) findViewById(R.id.difficultyreciever);
+        Intent receiverIntent = getIntent();
+        Double receivedValue = receiverIntent.getDoubleExtra("difficulty", 0.75);
+        if (receivedValue == 1.0) {
+            textView.setText("Difficulty: Hard");
+        } else if (receivedValue == 0.75) {
+            textView.setText("Difficulty: Medium");
+        } else {
+            textView.setText("Difficulty: Easy");
+        }
     }
 }
