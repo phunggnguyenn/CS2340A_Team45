45e2bbe97e71cdb735aa3664faa4721f3364a721
Maria Jothish <mariaphilipj@gmail.com>
2023-09-24 10:02:14 -0400

    Update HealthPoints on Game Screen

--- AndroidStudio_Template/app/src/main/java/com/example/demo_2340/MazeGameActivity.java
+++ AndroidStudio_Template/app/src/main/java/com/example/demo_2340/MazeGameActivity.java
@@ -8,19 +8,28 @@ import androidx.appcompat.app.AppCompatActivity;
 //launchMazeGameActivity() method in GameActivity.java will launch the maze background
 public class MazeGameActivity extends AppCompatActivity {
     TextView textView;
+    TextView textView2;
+    int healthPoints;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze); //will use the activity_maze xml background
         textView = (TextView) findViewById(R.id.difficultyreciever);
+        textView2 = (TextView) findViewById(R.id.healthpts);
         Intent receiverIntent = getIntent();
-        Double receivedValue = receiverIntent.getDoubleExtra("difficulty", 0.75);
-        if (receivedValue == 1.0) {
+        Double receivedDifficulty = receiverIntent.getDoubleExtra("difficulty", 0.75);
+        if (receivedDifficulty == 1.0) {
             textView.setText("Difficulty: Hard");
-        } else if (receivedValue == 0.75) {
+            healthPoints = 150;
+            textView2.setText("HP: 150");
+        } else if (receivedDifficulty == 0.75) {
             textView.setText("Difficulty: Medium");
+            healthPoints = 125;
+            textView2.setText("HP: 125");
         } else {
             textView.setText("Difficulty: Easy");
+            healthPoints = 100;
+            textView2.setText("HP: 100");
         }
     }
 }
