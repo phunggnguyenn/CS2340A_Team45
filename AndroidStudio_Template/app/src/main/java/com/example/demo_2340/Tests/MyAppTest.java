eb3653db0189dc1d2b28993feada984ced0b20f3
Maria Jothish <mariaphilipj@gmail.com>
2023-10-08 15:38:46 -0400

    Create test file

---
+++ AndroidStudio_Template/app/src/main/java/com/example/demo_2340/Tests/MyAppTest.java
@@ -0,0 +1,10 @@
+package com.example.demo_2340.Tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class MyAppTest {
+    @Test
+    public void testDifficulty(){
+
+    }
+}
