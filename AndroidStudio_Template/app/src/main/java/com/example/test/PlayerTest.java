d7969659551d5d79b5792b9104bd9e3aa22478f5
jwoo6310 <jwoo6310@gmail.com>
2023-10-11 20:11:32 -0400

    "Junit for Player class"

---
+++ AndroidStudio_Template/app/src/main/java/com/example/test/PlayerTest.java
@@ -0,0 +1,34 @@
+package com.example.test;
+
+import com.example.model.Player;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+// Tests the Player class
+public class PlayerTest {
+    private Player player1;
+    private Player player2;
+    @Before
+    public void setUp() {
+        player1 = Player.getInstance("Player 1", 100, 1);
+        player2 = Player.getInstance("Player 2", 200, 2);
+    }
+    @Test
+    public void testSingleton() {
+        // Check if both references point to the same instance
+        assertSame(player1, player2);
+    }
+    @Test
+    public void testValues() {
+        // Check if the properties are correctly set
+        assertEquals("Player 1", player1.getPlayerName());
+        assertEquals(100, player1.getHealthPoints());
+        assertEquals(1, player1.getAvatarId());
+
+        // Player 2 will be the same due to Singleton
+        assertEquals("Player 1", player2.getPlayerName());
+        assertEquals(100, player2.getHealthPoints());
+        assertEquals(1, player2.getAvatarId());
+    }
+}
