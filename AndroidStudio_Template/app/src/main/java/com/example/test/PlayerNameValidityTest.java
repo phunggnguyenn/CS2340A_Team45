af4b1b14145cd1828dee2ab9acfaea0eda694932
mgjslearn <mahag.student@gmail.com>
2023-10-19 12:16:16 -0400

    Update PlayerNameValidity UnitTests

--- AndroidStudio_Template/app/src/main/java/com/example/test/PlayerNameValidityTest.java
+++ AndroidStudio_Template/app/src/main/java/com/example/test/PlayerNameValidityTest.java
@@ -12,25 +12,19 @@ public class PlayerNameValidityTest {
         // Create a Player instance for testing
         player = Player.getInstance("PlayerName", 100, 1);
     }
-    //MEMBER: MAHATHI GUMUDAVELLI
-    //THIS METHOD TESTS When playername is neither null or empty
-    @Test
-    public void testPlayerNameNotNullOrEmpty() {
-        // Assert that the player name is not null or empty
-        assertFalse("Player name is not null or empty!", player.getPlayerName()== null || player.getPlayerName().isEmpty());
-    }
+
     //MEMBER: MAHATHI GUMUDAVELLI
     //THIS METHOD TESTS When playername is empty
     @Test
     public void testPlayerNameIsEmpty() {
-        player = Player.getInstance("", 100, 1);
-        assertFalse("Player Name is not empty!", player.getPlayerName().equals(""));
+        player = Player.getInstance("PlayerName", 100, 1);
+        assertFalse("Player Name is empty!", player.getPlayerName().isEmpty());
     }
     //MEMBER: MAHATHI GUMUDAVELLI
     //THIS METHOD TESTS When playername is null
     @Test
     public void testPlayerNameIsNull() {
-        player = Player.getInstance(null, 100, 1);
-        assertFalse("Player Name is not Null!", player.getPlayerName() == null);
+        player = Player.getInstance("PlayerName", 100, 1);
+        assertFalse("Player Name is Null!", player.getPlayerName() == null);
     }
 }
