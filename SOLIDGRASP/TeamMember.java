bf965aa423fdf095b6cf6fa687bcf86909e1f022
Maria Jothish <mariaphilipj@gmail.com>
2023-10-26 15:33:43 -0400

    Group Assignment to complete

---
+++ SOLIDGRASP/TeamMember.java
@@ -0,0 +1,11 @@
+public class TeamMember {
+    private String name;
+    private String email;
+
+    public TeamMember(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Other team member-related methods
+}
