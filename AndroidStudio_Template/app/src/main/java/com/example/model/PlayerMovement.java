f129049ea0708f89c25c3b72ee64172542f1856b
mgjslearn <mahag.student@gmail.com>
2023-10-27 18:11:56 -0400

    Fix Room changes

--- AndroidStudio_Template/app/src/main/java/com/example/model/PlayerMovement.java
+++ AndroidStudio_Template/app/src/main/java/com/example/model/PlayerMovement.java
@@ -8,34 +8,36 @@ import java.util.List;
 
 public class PlayerMovement implements PlayerMovementStrategy {
     private List<ImageView> blackTilesList;
+
     public PlayerMovement(List<ImageView> blackTilesList) {
         this.blackTilesList = blackTilesList;
     }
+
     @Override
     public void move(Player player, int keyCode) {
         switch (keyCode) {
-        case KeyEvent.KEYCODE_DPAD_DOWN:
-            player.setY(player.getY() + 10);
-            break;
-        case KeyEvent.KEYCODE_DPAD_UP:
-            player.setY(player.getY() - 10);
-            break;
-        case KeyEvent.KEYCODE_DPAD_LEFT:
-            player.setX(player.getX() - 10);
-            break;
-        case KeyEvent.KEYCODE_DPAD_RIGHT:
-            player.setX(player.getX() + 10);
-            break;
-        default:
-            return;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                player.setY(player.getY() + 10);
+                break;
+            case KeyEvent.KEYCODE_DPAD_UP:
+                player.setY(player.getY() - 10);
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                player.setX(player.getX() - 10);
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                player.setX(player.getX() + 10);
+                break;
+            default:
+                return;
         }
 
     }
+
     @Override
     public boolean isValidMove(List<ImageView> blackTilesList, int x, int y, Player player) {
         int playerRight = x + player.getPlayerWidth();
         int playerBottom = y + player.getPlayerHeight();
-<<<<<<< HEAD
         int blackTileWidth = 80;
         for (ImageView blackTile : blackTilesList) {
             int blackTileLeft = blackTile.getLeft();
@@ -45,31 +47,32 @@ public class PlayerMovement implements PlayerMovementStrategy {
             // not working as intended
             boolean xOverlap = playerRight > blackTileLeft && x < blackTileRight;
             boolean yOverlap = playerBottom > blackTileTop && y < blackTileBottom;
-=======
-        Rect playerRect = new Rect(x, y, x + 60, y + 60);
-        for (ImageView blackTile : blackTilesList) {
-            int blackTileLeft = blackTile.getLeft();
-            int blackTileTop = blackTile.getTop();
-            int blackTileRight = blackTile.getRight();
-            int blackTileBottom = blackTile.getBottom();
-            Rect blackTileRect = new Rect(blackTileLeft, blackTileTop, blackTileRight, blackTileBottom);
->>>>>>> main
+            Rect playerRect = new Rect(x, y, x + 60, y + 60);
+            for (ImageView blackTile2 : blackTilesList) {
+                blackTileLeft = blackTile.getLeft();
+                blackTileTop = blackTile.getTop();
+                blackTileRight = blackTile.getRight();
+                blackTileBottom = blackTile.getBottom();
+                Rect blackTileRect = new Rect(blackTileLeft, blackTileTop, blackTileRight, blackTileBottom);
 
-            if (playerRect.intersect(blackTileRect)) {
-                Log.d("Collision", "Intersection detected at X: " + x + ", Y: " + y);
-                Log.d("Collision", "Player Rect: " + playerRect.toString());
-                Log.d("Collision", "Black Tile Rect: " + blackTileRect.toString());
-                return false;
+                if (playerRect.intersect(blackTileRect)) {
+                    Log.d("Collision", "Intersection detected at X: " + x + ", Y: " + y);
+                    Log.d("Collision", "Player Rect: " + playerRect.toString());
+                    Log.d("Collision", "Black Tile Rect: " + blackTileRect.toString());
+                    return false;
+                }
             }
-
         }
+
         int screenWidth = 11 * (90);
         int screenHeight = 13 * (90);
         if (x < 0 || y < 0 || playerRight > screenWidth || playerBottom > screenHeight) {
             return false;
         }
-        // theres no collision, so valid move
+        // There's no collision, so valid move
         return true;
     }
-
 }
+
+
+
