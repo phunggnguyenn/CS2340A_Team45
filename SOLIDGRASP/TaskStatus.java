06c4034e69b00b790e646dfddc57c99089a38745
Phung Nguyen <phunggnuyenn@outlook.com>
2023-10-27 22:09:20 -0400

    Adding TaskStatus class

---
+++ SOLIDGRASP/TaskStatus.java
@@ -0,0 +1,11 @@
+/**
+ * @author: Phung Nguyen
+ * TaskStatus is a class that demonstrates low-coupling.
+ */
+public enum TaskStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED,
+    ON_HOLD,
+    CANCELED
+}
