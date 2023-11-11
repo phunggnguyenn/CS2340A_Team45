1306f7088e7fde76acc2f9eed27d699392fd7f30
mgjslearn <mahag.student@gmail.com>
2023-11-10 21:46:38 -0500

    fix merge conflicts

--- AndroidStudio_Template/app/src/main/java/com/example/model/BlueEnemy.java
+++ AndroidStudio_Template/app/src/main/java/com/example/model/BlueEnemy.java
@@ -6,25 +6,23 @@ import com.example.demo_2340.R;
 
 
 public class BlueEnemy extends Enemy {
-
-
-    public BlueEnemy(Context context, EnemyMovementStrategy movementStrategy) {
-        super(new ImageView(context), movementStrategy);
     //int blueenemy_x = 715;
     // int blueenemy_y = 65;
     private ImageView blueenemy;
 
     public BlueEnemy(Context context, int x, int y, EnemyMovementStrategy movementStrategy) {
         super(new ImageView(context), movementStrategy, x, y);
-
         getView().setImageResource(R.drawable.blueenemy);
-
+        getView().setX(x);
+        getView().setY(y);
     }
 
+    @Override
+    void generate() {
 
+    }
     public void move(EnemyMovementStrategy movementStrategy) {
         movementStrategy.move(this);
     }
 
 }
-
