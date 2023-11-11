da53b05e02a486b7cd1545a2a0c5e8ed96cd5338
mgjslearn <mahag.student@gmail.com>
2023-11-10 22:08:06 -0500

    Add Unit tests for enemy testing

---
+++ AndroidStudio_Template/app/src/main/java/com/example/test/EnemyTest.java
@@ -0,0 +1,45 @@
+package com.example.test;
+
+import com.example.model.BlueEnemy;
+import com.example.model.EnemyMovementStrategy;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnemyTest {
+    /**
+     * Member: Mahathi Gumudavelli
+     * This method tests blue enemy player movement
+     */
+    @Test
+    public void testMove() {  //testing blue enemy movement
+        int initialX = 50;
+        int initialY = 50;
+        EnemyMovementStrategy movementStrategy = enemy -> {
+            enemy.setX(enemy.getX() + 5);
+            enemy.setY(enemy.getY() + 5);
+        };
+        BlueEnemy blueEnemy = new BlueEnemy(null, initialX, initialY, movementStrategy);
+        blueEnemy.move();
+        assertEquals(initialX + 5, blueEnemy.getX());
+        assertEquals(initialY + 5, blueEnemy.getY());
+    }
+    @Test
+    public void testGetX() {
+        int initialX = 30;
+        int initialY = 40;
+        EnemyMovementStrategy movementStrategy = enemy -> { };
+        BlueEnemy blueEnemy = new BlueEnemy(null, initialX, initialY, movementStrategy);
+        int x = blueEnemy.getX();
+        assertEquals(initialX, x);
+    }
+    @Test
+    public void testGetY() {
+        int initialX = 60;
+        int initialY = 70;
+        EnemyMovementStrategy movementStrategy = enemy -> { };
+        BlueEnemy blueEnemy = new BlueEnemy(null, initialX, initialY, movementStrategy);
+        int y = blueEnemy.getY();
+        assertEquals(initialY, y);
+    }
+}
