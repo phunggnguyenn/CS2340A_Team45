1607330f9e2233b84f685bbccaed9c1ce629d794
mgjslearn <mahag.student@gmail.com>
2023-11-12 17:34:20 -0500

    modify enemy unit tests

--- AndroidStudio_Template/app/src/main/java/com/example/test/EnemyTest.java
+++ AndroidStudio_Template/app/src/main/java/com/example/test/EnemyTest.java
@@ -8,12 +8,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class EnemyTest {
-
+    /**
+     * Member: Mahathi Gumudavelli
+     * This method tests if green enemy implements strategy
+     */
     @Test
     public void testGreenEnemyImplementsEnemyMovementStrategy() {
         EnemyMovementStrategy greenEnemyMovementStrategy = new GreenEnemyMovementStrategy();
         assertTrue(greenEnemyMovementStrategy instanceof EnemyMovementStrategy);
     }
+    /**
+     * Member: Mahathi Gumudavelli
+     * This method tests if blue enemy implements strategy
+     */
     @Test
     public void testBlueEnemyImplementsEnemyMovementStrategy() {
         EnemyMovementStrategy blueEnemyMovementStrategy = new BlueEnemyMovementStrategy();
